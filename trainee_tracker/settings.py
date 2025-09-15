4f5c0bb12d46e28943a822247d5e384c7ba2fa1d
sugandv21 <sdvsuganya@gmail.com>
2025-09-15 15:34:15 +0530

    Update settings.py

--- trainee_tracker/settings.py
+++ trainee_tracker/settings.py
@@ -86,10 +86,7 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
+    "https://project-tracker-frontend-phi.vercel.app",
 ]
 
 
@@ -154,4 +151,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
